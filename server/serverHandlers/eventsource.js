e9437fe1dd5b49338642b19feefc28a3bd9d9d91
Hasan Yousef <hasan3ysf@users.noreply.github.com>
2016-09-07 10:09:21 +0300

    Update eventsource.js

--- server/serverHandlers/eventsource.js
+++ server/serverHandlers/eventsource.js
@@ -2,22 +2,20 @@
 |*|  EvenSource Server / Server-Sent Events (SSE)
 \*/
 var EventSource = require('faye-websocket').EventSource
+var getInv      = require("../modules/getInv.js")
 
 exports.handleReq = function (req, res) {
        console.log('ess')
        var path = req.url,
        es   = new EventSource(req, res, { headers: { 'Access-Control-Allow-Origin': '*' } } );
+       getInv.start()
        switch (path.indexOf("getinv")){
            case 1: 
                 console.log('1')
-                sse(es)
+                getInv.data(es)
                 break
            default: 
                 console.log('wrong event')
                 break
         }
-}
-
-sse = function (es) {
-	if (es) es.send(JSON.stringify({'name':'Hasan'}), {event: 'name', id: (new Date()).toLocaleTimeString()})
 }
