78e0f7b564903df340c856a4d5166e5c21119b30
Hasan Yousef <hasan3ysf@users.noreply.github.com>
2016-09-07 10:24:07 +0300

    Update eventsource.js

--- server/serverHandlers/eventsource.js
+++ server/serverHandlers/eventsource.js
@@ -4,14 +4,12 @@
 var EventSource = require('faye-websocket').EventSource
 var getInv      = require("../modules/getInv.js")
 
-exports.handleReq = function (req, res) {
-       console.log('ess')
-       var path = req.url,
+exports.handleReq = function (req, res, path) {
        es   = new EventSource(req, res, { headers: { 'Access-Control-Allow-Origin': '*' } } );
        getInv.start()
-       switch (path.indexOf("getinv")){
-           case 1: 
-                console.log('1')
+       console.log(path);
+       switch (path){
+           case "/getinv": 
                 getInv.data(es)
                 break
            default: 
